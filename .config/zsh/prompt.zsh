78aad7057908880134a5971c5df0f696f5701866
Ari RERA 43 <arirera43@gmail.com>
2024-12-07 20:32:17 -0500

    Minor prompt changes
    - Update prompt (clock) on newline
    - Move prompts to individual scripts

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,29 +1,26 @@
 # Prompts
 ## Fix broken output
 autoload -Uz add-zsh-hook
-function reset_broken_terminal () {
+function _reset-broken-terminal () {
 	printf '%b' '\e[0m\e(B\e)0\017\e[?5l\e7\e[0;0r\e8'
 }
-add-zsh-hook -Uz precmd reset_broken_terminal
+add-zsh-hook -Uz precmd _reset-broken-terminal
+function _on-accept-line {
+	_reset-broken-terminal
+	zle reset-prompt
+	zle .accept-line # `.` calls the default accept-line function
+}
+zle -N accept-line _on-accept-line # Replace accept-line with my own
 
 # Human readable error codes
-ecp () {
+function ecp () {
 	~/.config/shell/errorcodeparser $1
 }
-
 
 
 # Short directory listing (currently not used)
 # sourcing this file defines the `spwd` function
 sourceife $HOME/.config/shell/shortpwd
 
-## My Prompt
-autoload -Uz promptinit
-promptinit
-fpath=("$HOME/.zprompts" "$fpath[@]")
-prompt_custom_setup() {
-	PS1='%F{27}'$'\ue0c7''%K{27}%F{15} %n %K{39}%F{27}'$'\ue0b0'' %F{15}%~ %k%F{39}'$'\ue0b0''%f%k '
-	RPROMPT='%F{207}'$'\ue0b2''%K{207}%F{15} $(ecp $?) %F{201}'$'\ue0b2''%K{201}%F{15} '"$($HOME/.config/shell/detectbox)"' %F{165}'$'\ue0b2''%K{165}%F{15} %* %k%F{165}'$'\ue0c6''%f%k'
-}
-prompt_themes+=( custom )
-prompt custom
+sourceife $HOME/.config/zsh/prompts/*
+prompt slick-arrows
