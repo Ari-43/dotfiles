57d31cf9a1db03019b145cfbb5dcd9fdeb2a2bd7
Ari RERA 43 <arirera43@gmail.com>
2024-12-18 20:40:49 -0500

    Make zsh syntax highlighting work on all platforms

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -1,4 +1,5 @@
 setopt autocd correct histignorespace PROMPT_SUBST
+
 # Shell completion & correction
 zstyle :compinstall filename '/home/ari/.config/zsh/.zshrc'
 autoload -Uz compinit bashcompinit select-word-style
@@ -8,11 +9,16 @@ zstyle ':completion::complete:*' gain-privileges 1
 zstyle ':completion:*' menu select
 compinit
 bashcompinit
- 
 
 ENABLE_CORRECTION="true"
-sourceife /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-sourceife /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+sourceife /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # Arch
+sourceife /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # Debian
+sourceife /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # BSD
+sourceife $(brew --prefix 2> /dev/null)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh # macOS Brew
+
+sourceife /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh # Arch
+sourceife /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh # Debian
 
 # Disable beep
 unsetopt beep
