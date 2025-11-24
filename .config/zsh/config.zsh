84b8f4dd820dbc36366943f9c74aa62040c88517
Ari43 <arirera43@gmail.com>
2025-11-24 13:00:26 -0500

    zsh: minor line editing changes

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -4,9 +4,10 @@ setopt autocd correct histignorespace PROMPT_SUBST
 zstyle :compinstall filename '/home/ari/.config/zsh/.zshrc'
 autoload -Uz compinit bashcompinit select-word-style
 select-word-style bash
-zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Za-z}'
+zstyle ':completion:*' matcher-list 'm:{A-Za-z}={A-Za-z}'
 zstyle ':completion::complete:*' gain-privileges 1
 zstyle ':completion:*' menu select
+zstyle ':completion:*' use-ip true
 compinit
 bashcompinit
 
@@ -19,6 +20,12 @@ unsetopt beep
 
 # This does... something
 bindkey -e
+
+# Ctrl+D to clear line
+# https://wiki.archlinux.org/title/Zsh#Shortcut_to_exit_shell_on_partial_command_line
+exit_zsh() { exit }
+zle -N exit_zsh
+bindkey '^D' exit_zsh
 
 # zsh truecolor support
 # https://wiki.archlinux.org/title/zsh#Colors
